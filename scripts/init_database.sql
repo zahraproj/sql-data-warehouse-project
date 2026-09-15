ff572b9223cecb84636830b534dbfb36c65f201e
zahraproj <zahra2alsaeed@gmail.com>
2026-09-15 20:24:03 +0300

    Create init_database.sql to set up data warehouse
    This script initializes the database and creates the necessary schemas.

---
+++ scripts/init_database.sql
@@ -0,0 +1,15 @@
+-- ADD commenits here 
+-- Create DW
+
+USE master;
+
+CREATE DATABASE datawarehouse;
+
+USE datawarehouse;
+
+CREATE SCHEMA bronze;
+go
+CREATE SCHEMA silver;
+go
+CREATE SCHEMA gold;
+go
